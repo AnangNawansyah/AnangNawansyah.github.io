7859922b978b583154447992ae2b807edd6d2d97
Nawaan <anang.nawansyah@gmail.com>
2022-01-19 21:25:51 +0700

    update page

--- js/main.js
+++ js/main.js
@@ -85,3 +85,10 @@
     }
   });
 })();
+
+window.addEventListener("load", () => {
+  document.querySelector(".preloader").classList.add("fade-out");
+  setTimeout(() => {
+    document.querySelector(".preloader").style.display = "none";
+  }, 600);
+});
